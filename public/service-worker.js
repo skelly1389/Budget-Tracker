d9d8574d6d6e3f30307677487cbf094a18649f78
Sean Kelly <invertedrdm@gmail.com>
2021-04-28 18:11:09 -0400

    testing service worker stuff

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,30 @@
-console.log("sup");
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/db.js",
+  "/manifest.webmanifest",
+  "/style.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('static').then( cache => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+    console.log('Install');
+    self.skipWaiting();
+  });
+
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then( response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
