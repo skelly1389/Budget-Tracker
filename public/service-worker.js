3274b1a61a8901f22589221c23aa8a378121bfbc
Sean Kelly <invertedrdm@gmail.com>
2021-04-28 20:15:01 -0400

    trimmed some too whoops

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,7 +21,6 @@ self.addEventListener("install", function (evt) {
   self.skipWaiting();
 });
 
-// activate
 self.addEventListener("activate", function(evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
@@ -39,14 +38,12 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 
-// fetch
 self.addEventListener("fetch", function(evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
           .then(response => {
-            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
             }
@@ -54,7 +51,6 @@ self.addEventListener("fetch", function(evt) {
             return response;
           })
           .catch(err => {
-            // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
       }).catch(err => console.log(err))
